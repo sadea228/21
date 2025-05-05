dc5a6cd332206c04a25f98c716e6c6625b9cc767
sadea228 <you@example.com>
2025-05-05 20:16:23 +0300

    Обновление config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7825658711:AAGglEpeH55SoLAthkkGUuh0A1AGJH_1R2o")
 
 # Конфигурация для webhook (Render)
-WEBHOOK_HOST = os.getenv("WEBHOOK_URL", "https://two1-hu47.onrender.com")
-WEBHOOK_PATH = os.getenv("WEBHOOK_PATH", "/webhook/")
+WEBHOOK_HOST = os.getenv("WEBHOOK_HOST", "https://two1-hu47.onrender.com")
+WEBHOOK_PATH = os.getenv("WEBHOOK_PATH", "/webhook")
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 # Параметры веб-сервера
